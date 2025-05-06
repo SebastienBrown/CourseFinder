702fff5e9516b963d23f2d4e8a0dc04af9f545c0
hnakazawa <haru51.nakazawa@gmail.com>
2025-05-06 01:35:06 -0400

    Cleaned 2021F-2324S with LLM

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # CourseFinder
+
+# Creating the virtual environment:
+- `python3.10 -m venv course_venv`
+- `source course_venv/bin/activate`
+- `pip install requirements.txt`
+
+# Workflow:
+
+**Data Collection and Cleaning**
+1. Scraping:
+```sbatch ./CourseFinder/course_scraper.sbatch```
+
+2. To complete the scraping, 
+- ```sbatch ./CourseFinder/failed_links.sbatch```
+- Manually check the log of failed_links and add courses
+- Find course AMST 224 in amherst_courses_2526F.json and manually add course codes EDST/PSYC/AMST/AAPI 224
+
+3. To clean scraped data:
+- ```sbatch ./CourseFinder/llm_parsing.sbatch```
+- Manually clean the courses printed under `Courses that caused errors`
+
+**Network Graph**
--- llm_parsing.py
+++ llm_parsing.py
@@ -4,6 +4,7 @@ import re
 from llm_client import llm
 from langchain_core.prompts import PromptTemplate
 
+os.chdir("/orcd/home/002/hnaka24/CourseFinder/")
 input_folder = "scraped_test"
 output_folder = "llm_cleaned"
 os.makedirs(output_folder, exist_ok=True)
@@ -14,10 +15,11 @@ You are a precise cleaner of college course descriptions.
 Only remove any non-academic information, including:
 - professor names or titles
 - semester offering (e.g., "Fall semester", "Spring semester", "January term")
+- course codes (e.g., "(Offered as ...)")
 - prerequisites, enrollment limits, or course requirements
 - teaching format (e.g., "discussion-based", "lectures", "conducted in person")
-- any logistical information (e.g., location, sections)
-- information about not open to students who have taken other courses                                       
+- any logistical information (e.g., location, sections, costs)
+- information about not open to certain students (e.g., who have taken other courses, first year students, certain majors)
 
 
 Do not paraphrase, reword, or summarize.
@@ -55,19 +57,32 @@ def clean_description(raw_description):
         return raw_description
 
 
-
+errors = []
 for filename in os.listdir(input_folder):
-    if not filename.endswith(".json"):
+    if not (filename.startswith("amherst") and filename.endswith(".json")):
         continue
 
-    with open(os.path.join(input_folder, filename)) as f:
-        courses = json.load(f)
+    with open(os.path.join(input_folder, filename), "rb") as f:
+        raw = f.read()
+
+    text = raw.decode("utf-8", errors="ignore")  # Skip invalid characters
+    courses = json.loads(text)
+
+    # with open(os.path.join(input_folder, filename)) as f:
+    #     courses = json.load(f)
 
     for course in courses:
         if "description" in course:
-            course["description"] = clean_description(course["description"])
+            try: 
+                course["description"] = clean_description(course["description"])
+            except:
+                errors.append((course.get('semester'), course.get('course_title')))
 
     with open(os.path.join(output_folder, filename), "w") as f:
         json.dump(courses, f, indent=2)
 
 print("Descriptions cleaned and saved to 'llm_cleaned/'")
+
+print("\n\nCourses that caused errors:")
+for err in errors:
+    print(err)
