111f394cd12bf1d88781d00710fdf09fbe2db46c
hnaka24 <hnakazawa@povertyaction.org>
2025-05-11 18:16:52 -0400

    Removed the 499, 490, 390 courses

--- course-visualization/src/App.js
+++ course-visualization/src/App.js
@@ -4,15 +4,15 @@ import CourseSimilarityTSNEGraph from './CourseSimilarityTSNEGraph';
 import CourseSimilarityPrecomputedGraph from './CourseSimilarityPrecomputedGraph';
 
 function App() {
-  const [mode, setMode] = useState('pca');
+  const [mode, setMode] = useState('precomputed-tsne');
   return (
     <div className="App">
-      <div style={{ position: 'absolute', top: 10, left: 10, zIndex: 10 }}>
+      {/* <div style={{ position: 'absolute', top: 10, left: 10, zIndex: 10 }}> */}
        {/*  <button onClick={() => setMode('pca')} style={{ marginRight: 8, padding: '0.5em 1em', fontWeight: mode === 'pca' ? 'bold' : 'normal' }}>PCA</button>
         <button onClick={() => setMode('tsne')} style={{ marginRight: 8, padding: '0.5em 1em', fontWeight: mode === 'tsne' ? 'bold' : 'normal' }}>t-SNE</button>
         <button onClick={() => setMode('precomputed-pca')} style={{ marginRight: 8, padding: '0.5em 1em', fontWeight: mode === 'precomputed-pca' ? 'bold' : 'normal' }}>Precomputed PCA</button> */}
-        <button onClick={() => setMode('precomputed-tsne')} style={{ padding: '0.5em 1em', fontWeight: mode === 'precomputed-tsne' ? 'bold' : 'normal' }}>Precomputed t-SNE</button>
-      </div>
+        {/* <button onClick={() => setMode('precomputed-tsne')} style={{ padding: '0.5em 1em', fontWeight: mode === 'precomputed-tsne' ? 'bold' : 'normal' }}>Precomputed t-SNE</button> */}
+      {/* </div> */}
       {/* {mode === 'pca' && <CourseSimilarityGraph />}
       {mode === 'tsne' && <CourseSimilarityTSNEGraph />}
       {mode === 'precomputed-pca' && <CourseSimilarityPrecomputedGraph mode="pca" />} */}
