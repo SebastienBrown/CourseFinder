ad6c645ccf92bcb6c187598425e93db5e66e41a7
hnakazawa <haru51.nakazawa@gmail.com>
2025-05-31 00:04:33 -0400

    Cleaning code and updates to process all sems

--- append_metadata.py
+++ append_metadata.py
@@ -2,7 +2,7 @@ import json
 import os
 
 # List of semesters to process
-semesters = ['2223F', '2223S', '2324F', '2324S']
+semesters = ['0910F', '0910S', '1011F', '1011S', '1112F', '1112S', '1213F', '1213S', '1314F', '1314S', '1415F', '1415S', '1516F', '1516S', '1617F', '1617S', '1718F', '1718S', '1819F', '1819S', '1920F', '1920S', '2021F', '2021J', '2021S', '2122F', '2122J', '2122S', '2223F', '2223S', '2324F', '2324S', '2425F', '2425S', '2526F', '2526S']
 
 # Initialize empty list to store all courses
 all_courses = []
