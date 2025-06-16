699938c55c6a57e757313ec541519dfc03c43ffe
SebastienBrown <107078090+SebastienBrown@users.noreply.github.com>
2025-06-15 23:15:18 -0400

    x

--- backend/schedule.py
+++ backend/schedule.py
@@ -333,4 +333,4 @@ def transcript_parsing():
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", PORT)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
