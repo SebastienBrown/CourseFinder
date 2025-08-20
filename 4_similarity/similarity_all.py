05a1399e2b53c04d4ca4f61d9be359fdfe9ff2f6
hnakazawa <haru51.nakazawa@gmail.com>
2025-08-20 19:08:50 +0700

    Organized similarity code

--- 4_similarity/similarity_all.py
+++ 4_similarity/similarity_all.py
@@ -5,15 +5,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 # ==== Configuration ====
 dropbox = '/Users/hnaka24/Dropbox (Personal)/AmherstCourses/'
 code = '/Users/hnaka24/Desktop/code/CourseFinder/'
-model = "sbert"
-mode = "off_the_shelf"
+model = "gpt" # "gpt" "sbert"
+mode = "off_the_shelf" # "self_supervised" "off_the_shelf"
 
-# semesters = ['0910F', '0910S', '1011F', '1011S', '1112F', '1112S', '1213F', '1213S', '1314F', '1314S', '1415F', '1415S', '1516F', '1516S', '1617F', '1617S', '1718F', '1718S', '1819F', '1819S', '1920F', '1920S', '2021F', '2021J', '2021S', '2122F', '2122J', '2122S', '2223F', '2223S', '2324F', '2324S', '2425F', '2425S', '2526F', '2526S']
+semesters = ['0910F', '0910S', '1011F', '1011S', '1112F', '1112S', '1213F', '1213S', '1314F', '1314S', '1415F', '1415S', '1516F', '1516S', '1617F', '1617S', '1718F', '1718S', '1819F', '1819S', '1920F', '1920S', '2021F', '2021J', '2021S', '2122F', '2122J', '2122S', '2223F', '2223S', '2324F', '2324S', '2425F', '2425S', '2526F', '2526S']
 # embeddings_path = 'embeddings/'
-semesters = ['2324S']
 embeddings_path = dropbox + f'data/2_intermediate/2_embeddings/{model}_{mode}/'
 
-output_file = dropbox + f'data/2_intermediate/3_similarity/{model}_{mode}/output_similarity_2324S.json'
+output_file = dropbox + f'data/2_intermediate/3_similarity/{model}_{mode}/output_similarity_all.json'
 
 # ==== Functions ====
 def compute_cosine_similarity(embedding1, embedding2):
